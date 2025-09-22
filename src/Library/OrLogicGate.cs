8244aaf454d877cb9ed25e8f8c675253cd005378
axelferreira100 <GUSTAVO.FERREIRA@correo.ucu.edu.uy>
2025-09-22 11:05:28 -0300

    Finished Or Logic Gate.

--- src/Library/OrLogicGate.cs
+++ src/Library/OrLogicGate.cs
@@ -23,4 +23,28 @@ public class OrLogicGate
         get { return this.resultValue; }
         private set { this.resultValue = value; }
     }
+
+    public OrLogicGate(string name)
+    {
+        this.Name = name;
+        this.Inputs = new List<int>();
+        this.ResultValue = 0;
+    }
+    
+    public void AddInput(int input)
+    {
+        this.Inputs.Add(input);
+    }
+
+    public void EvaluateExpression()
+    {
+        if (this.Inputs.Contains(1))
+        {
+            this.ResultValue = 1;
+        }
+        else
+        {
+            this.ResultValue = 0;
+        }
+    }
 }
