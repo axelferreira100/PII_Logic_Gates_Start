f81d2c4a0d0decfe84920777c36c6c2777f9a20e
axelferreira100 <GUSTAVO.FERREIRA@correo.ucu.edu.uy>
2025-09-23 21:16:38 -0300

    Finished Or Logic Gate Tests.

--- test/Library.Tests/OrLogicGateTests.cs
+++ test/Library.Tests/OrLogicGateTests.cs
@@ -5,7 +5,7 @@ namespace Library.Tests;
 public class OrLogicGateTests
 {
     private OrLogicGate _orLogicGate;
-    
+
     [SetUp]
     public void Setup()
     {
@@ -17,9 +17,9 @@ public class OrLogicGateTests
     {
         string expectedName = "OR-2";
         int expectedInputsElementsCount = 0;
-        
+
         OrLogicGate orLogicGate = new OrLogicGate("OR-2");
-        
+
         string actualName = orLogicGate.Name;
         List<ILogicGate> actualListOfInputs = orLogicGate.Inputs;
         int actualInputsElementsCount = actualListOfInputs.Count;
@@ -33,13 +33,13 @@ public class OrLogicGateTests
     public void AddInput_ValidInput_AddedInputToTheListOfInputsCorrectly()
     {
         int expectedListElementsCount = 1;
-        
         FalseLogicGate falseLogicGate = new FalseLogicGate();
+
         _orLogicGate.AddInput(falseLogicGate);
 
         List<ILogicGate> actualInputs = _orLogicGate.Inputs;
         int actualListElementsCount = actualInputs.Count;
-                
+
         Assert.IsNotEmpty(actualInputs);
         Assert.AreEqual(expectedListElementsCount, actualListElementsCount);
         Assert.Contains(falseLogicGate, actualInputs);
@@ -62,11 +62,25 @@ public class OrLogicGateTests
     }
 
     [Test]
+    public void RemoveInput_InputNotInList_DoesNothing()
+    {
+        TrueLogicGate trueLogicGate = new TrueLogicGate();
+
+        _orLogicGate.RemoveInput(trueLogicGate);
+        
+        Assert.IsEmpty(_orLogicGate.Inputs);
+    }
+
+    [Test]
     [TestCase(0, 0, 0)]
     [TestCase(0, 1, 1)]
     [TestCase(1, 0, 1)]
     [TestCase(1, 1, 1)]
-    public void EvaluateExpression_ValidCasesOrTruthTable_ReturnsExpectedValues(int input1, int input2, int expectedResult)
+    public void EvaluateExpression_ValidCasesOrTruthTableWithTwoInputs_ReturnsExpectedValues(
+        int input1,
+        int input2,
+        int expectedResult
+    )
     {
         ILogicGate newInput1 = input1 == 0 ? new FalseLogicGate() : new TrueLogicGate();
         ILogicGate newInput2 = input2 == 0 ? new FalseLogicGate() : new TrueLogicGate();
@@ -74,7 +88,36 @@ public class OrLogicGateTests
         _orLogicGate.AddInput(newInput2);
 
         int obtainedResult = _orLogicGate.EvaluateExpression();
+
+        Assert.AreEqual(expectedResult, obtainedResult);
+    }
+
+    [Test]
+    [TestCase(0, 0, 0, 0)]
+    [TestCase(0, 0, 1, 1)]
+    [TestCase(0, 1, 0, 1)]
+    [TestCase(0, 1, 1, 1)]
+    [TestCase(1, 0, 0, 1)]
+    [TestCase(1, 0, 1, 1)]
+    [TestCase(1, 1, 0, 1)]
+    [TestCase(1, 1, 1, 1)]
+    
+    public void EvaluateExpression_ValidCasesOrTruthTableWithThreeInputs_ReturnsExpectedValues(
+        int input1,
+        int input2,
+        int input3,
+        int expectedResult
+    )
+    {
+        ILogicGate newInput1 = input1 == 0 ? new FalseLogicGate() : new TrueLogicGate();
+        ILogicGate newInput2 = input2 == 0 ? new FalseLogicGate() : new TrueLogicGate();
+        ILogicGate newInput3 = input3 == 0 ? new FalseLogicGate() : new TrueLogicGate();
+        _orLogicGate.AddInput(newInput1);
+        _orLogicGate.AddInput(newInput2);
+        _orLogicGate.AddInput(newInput3);
+
+        int obtainedResult = _orLogicGate.EvaluateExpression();
         
-        Assert.AreEqual(obtainedResult, expectedResult);
+        Assert.AreEqual(expectedResult, obtainedResult);
     }
 }
